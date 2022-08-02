19841bfdf9b3aaffadbb6b11d1b9b7e7b750276e
shriramrav <shriram.ravishankarsr@gmail.com>
2022-08-02 15:38:30 -0400

    Reworking socket and extension info storage code

--- src/content.js
+++ src/content.js
@@ -1,25 +1,41 @@
 import * as m from "./modules/messages";
-// import { rejectErrors } from "./modules/utility";
-import { registerUser } from "./modules/socket";
+import { rejectErrors } from "./modules/utility";
+import { registerUser, sync } from "./modules/socket";
 
 let port = chrome.runtime.connect(m.connectInfo);
 let socket;
 
 //
 
-async function onMessageEventListener(message) {
+async function portMessageHandler(message) {
   let caseTriggered = true;
 
   switch (message.request) {
     case m.server.registerUser.request:
-      let newData = await registerUser(socket, message.data);
+      // socket = new WebSocket(`wss://${m.servers[1]}.glitch.me/`);
+      let newData;
 
-      console.log('newData');
+      [socket, newData] = await registerUser(message.data);
+
+
+      console.log(`inside switch:`);
+      console.log(socket);
+
+      console.log("second entry");
       console.log(newData);
+
+      if (newData.data !== m.server.events.error) {
+        sync(socket);
+      }
 
       message.data = newData;
       break;
 
+    case m.server.destroy.request:
+      //
+      window.removeEventListener("message", windowMessageHandler);
+      console.log("destroy event triggered");
+      break;
     default:
       console.log("case not triggered, the following is the message:");
       console.log(message);
@@ -27,11 +43,20 @@ async function onMessageEventListener(message) {
   }
 
   if (caseTriggered) {
-    port.postMessage(message);
+    rejectErrors(() => port.postMessage(message));
   }
 }
 
+let windowMessageHandler = (m) => portMessageHandler(m.data)
+
+
+window.addEventListener("message", (m) => {
+  console.log("hello, from content");
+  console.log(m);
+});
+
 // Event listeners
-port.onMessage.addListener(onMessageEventListener);
+port.onMessage.addListener(portMessageHandler);
+window.addEventListener("message", windowMessageHandler);
 
 console.log("done");
